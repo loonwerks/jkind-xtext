c80716618978ee05c96e6aa0f50677688718e1d7
Andrew Gacek <andrew.gacek@gmail.com>
2014-12-04 15:27:34 -0600

    Print invariants to console

--- jkind.xtext.ui/src/jkind/xtext/ui/views/JKindMenuListener.java
+++ jkind.xtext.ui/src/jkind/xtext/ui/views/JKindMenuListener.java
@@ -2,6 +2,7 @@ package jkind.xtext.ui.views;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import jkind.api.results.PropertyResult;
 import jkind.api.ui.results.AnalysisResultTable;
@@ -9,6 +10,7 @@ import jkind.results.Counterexample;
 import jkind.results.InvalidProperty;
 import jkind.results.Property;
 import jkind.results.UnknownProperty;
+import jkind.results.ValidProperty;
 import jkind.results.layout.Layout;
 
 import org.eclipse.jface.action.Action;
@@ -17,8 +19,17 @@ import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.program.Program;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IConsoleView;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
 
 public class JKindMenuListener implements IMenuListener {
 	private final IWorkbenchWindow window;
@@ -45,6 +56,7 @@ public class JKindMenuListener implements IMenuListener {
 
 	private void addLinkedMenus(IMenuManager manager, PropertyResult result) {
 		addViewCounterexampleMenu(manager, result);
+		addViewInvariantsMenu(manager, result);
 	}
 
 	private void addViewCounterexampleMenu(IMenuManager manager, PropertyResult result) {
@@ -100,6 +112,64 @@ public class JKindMenuListener implements IMenuListener {
 			return ((UnknownProperty) prop).getInductiveCounterexample();
 		} else {
 			return null;
+		}
+	}
+
+	private void addViewInvariantsMenu(IMenuManager manager, PropertyResult result) {
+		if (!(result.getProperty() instanceof ValidProperty)) {
+			return;
+		}
+		ValidProperty vp = (ValidProperty) result.getProperty();
+
+		if (vp.getInvariants().isEmpty()) {
+			return;
+		}
+
+		manager.add(new Action("View invariants in console") {
+			@Override
+			public void run() {
+				viewInvariantsConsole(vp.getName(), vp.getInvariants());
+			}
+		});
+	}
+
+	private void viewInvariantsConsole(String property, List<String> invariants) {
+		MessageConsole console = findConsole("JKind Invariants");
+		showConsole(console);
+		console.clearConsole();
+
+		try (MessageConsoleStream stream = console.newMessageStream()) {
+			stream.println("Invariants for " + property + ": ");
+			for (String invariant : invariants) {
+				stream.println("  " + invariant);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static MessageConsole findConsole(String name) {
+		ConsolePlugin plugin = ConsolePlugin.getDefault();
+		IConsoleManager conMan = plugin.getConsoleManager();
+		IConsole[] existing = conMan.getConsoles();
+		for (int i = 0; i < existing.length; i++) {
+			if (name.equals(existing[i].getName())) {
+				return (MessageConsole) existing[i];
+			}
+		}
+		// no console found, so create a new one
+		MessageConsole myConsole = new MessageConsole(name, null);
+		conMan.addConsoles(new IConsole[] { myConsole });
+		return myConsole;
+	}
+
+	private void showConsole(IConsole console) {
+		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		try {
+			IConsoleView view = (IConsoleView) page.showView(IConsoleConstants.ID_CONSOLE_VIEW);
+			view.display(console);
+			view.setScrollLock(true);
+		} catch (PartInitException e) {
 		}
 	}
 }
